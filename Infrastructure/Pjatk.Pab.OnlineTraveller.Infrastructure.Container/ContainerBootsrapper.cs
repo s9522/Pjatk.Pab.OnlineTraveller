41affd7e8cb3fd51a7f80e0629250162322de542
smiodek <smiodek@172.23.129.49>
2015-04-19 18:15:18 +0200

    zalążek kontenera DI

---
+++ Infrastructure/Pjatk.Pab.OnlineTraveller.Infrastructure.Container/ContainerBootsrapper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Unity;
+using Pjatk.Pab.OnlineTraveller.Infrastructure.Container.Modules;
+
+namespace Pjatk.Pab.OnlineTraveller.Infrastructure.Container
+{
+    public static class ContainerBootsrapper
+    {
+        public static void RegisterComponents()
+        {
+            IUnityContainer container = new UnityContainer();
+            RepositoriesModule.RegisterTypes(container);
+        }
+    }
+}
---
+++ Infrastructure/Pjatk.Pab.OnlineTraveller.Infrastructure.Container/Modules/RepositoriesModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+using Pjatk.Pab.OnlineTraveller.Core.Interfaces.Repositories;
+using Pjatk.Pab.OnlineTraveller.Infrastructure.Data.Repositories;
+
+namespace Pjatk.Pab.OnlineTraveller.Infrastructure.Container.Modules
+{
+    public static class RepositoriesModule
+    {
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            container.RegisterType<IUnitOfWork, UnitOfWork>();
+            container.RegisterType(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
